ae4ad835307ed651a0ab61dc3f0e84584fbadcfb
Charles <charles.griego18@gmail.com>
2019-11-08 14:52:06 -0500

    More updates to test_molecule.py

--- molecool/tests/test_molecule.py
+++ molecool/tests/test_molecule.py
@@ -1,13 +1,16 @@
+"""
+Test for the molecule module
+"""
 import pytest
 
 import molecool
 
-def test_molecular_mass(test_molecule):
-    symbols = ['C', 'H', 'H', 'H', 'H']
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H']
     
-    calculated_mass = molecool.calculate_molecular_mass(symbols)
+    calculated_mass = molecool.molecule.calculate_molecular_mass(symbols)
 
-    actual_mass = molecool.atom_data.atom_weights['C'] + molecool.atom_data.atom_weights['H'] +\
-         molecool.atom_data.atom_weights['H'] + molecool.atom_data.atom_weights['H']
+    actual_mass = molecool.atom_data.atomic_weights['C'] + molecool.atom_data.atomic_weights['H'] +\
+         molecool.atom_data.atomic_weights['H'] + molecool.atom_data.atomic_weights['H']
     
     assert actual_mass == calculated_mass
